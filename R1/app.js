6be3c031cec17384b0000520a1fea3d66590fd9f
Per Thoresson <per@thoresson.com.au>
2019-06-06 00:38:27 +1000

    prepping R1 app
    prepping R1 app

--- R1/app.js
+++ R1/app.js
@@ -13,3 +13,37 @@ server.listen(port, () => {
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+io.on('connection', (socket) => {
+    var addedUser = false;
+  
+    // when the client emits 'new message', this listens and executes
+    socket.on('UI state change', (data) => {
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    });
+  
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      });
+    });
+  
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      });
+    }); 
+  
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+      
+    });
+});
