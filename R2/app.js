f30d78cae5fe0aa8742b7e4d2e92ee80aaa87de5
Per Thoresson <per@thoresson.com.au>
2019-06-05 20:40:47 +1000

    Adding Express dependency
    Adding Express dependency

---
+++ R2/app.js
@@ -0,0 +1,15 @@
+
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
