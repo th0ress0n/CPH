0d1fc942a95f278fea4930da7ea7d6ed691c403d
th0ress0n <per@thoresson.com.au>
2019-06-09 20:03:25 +1000

    Audio and Init sequence
    Completed setup of Howler. Added the audio sequence.
    Sorted the Socket disconnet issue.

--- R1/app.js
+++ R1/app.js
@@ -1,3 +1,27 @@
+var Constants = {
+    STATE_INIT                              : "INIT",
+    STATE_ROOM_READY                        : "ROOM READY",
+    STATE_ROOM_ENTERED                      : "ROOM ENTERED",
+    STATE_ROOM_ENTERED_UNSEATED             : "ROOM ENTERED UNSEATED",
+    STATE_ROOM_USER_SEATED                  : "ROOM USER SEATED",
+    STATE_ROOM_USER_SEATED_PHOTO_DONE       : "ROOM USER SEATED PHOTO DONE",
+    STATE_ROOM_USER_SEATED_INACTIVE         : "ROOM USER SEATED INACTIVE",
+    STATE_ROOM_TV_ACTIVATED                 : "ROOM TV ACTIVATED",
+    STATE_ROOM_VIDEO_PLAYING                : "ROOM VIDEO PLAYING",
+    STATE_ROOM_VIDEO_FINISHED               : "ROOM VIDEO FINISHED",
+    STATE_ROOM_EXIT_SEQUENCE                : "ROOM EXIT SEQUENCE",
+    
+    SENSOR_DOOR_TRIGGERED                   : "DOOR SENSOR TRIGGERED",
+    SENSOR_SEATING_TRIGGERED                : "SEATING SENSOR TRIGGERED",
+
+    API_BASE_PATH                           : "",
+    MODE_DEBUG                              : true,
+
+    SENSOR_DOOR_TOLERANCE_DIST              : 3000,
+    SENSOR_DOOR_TOL_DURATION                : 1200,
+
+    SENSOR_SEAT_TOLERANCE_DIST              : 3000
+}
 
 // Setup basic express server
 var express = require('express');
@@ -8,43 +32,78 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+var sockRef = null;
 
-// Routing
+// GPIO !!
+var gpio = require("gpio");
+
+// start listening....
+server.listen(port, () => { console.log('Server listening at port %d', port) });
+
+// Routing -  Load UI from public root
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+// setup the socket...
 io.on('connection', (socket) => {
-    var addedUser = false;
-  
-    // when the client emits 'new message', this listens and executes
-    socket.on('UI state change', (data) => {
-      // we tell the client to execute 'new message'
-      socket.broadcast.emit('new message', {
-        username: socket.username,
-        message: data
-      });
+
+    sockRef = socket
+
+    // Bounce to startup ->
+    socket.on('startup', (data) => {
+        console.log("startup")
+        socket.broadcast.emit('init', {
+            init: true
+        });
     });
-  
-    // when the client emits 'typing', we broadcast it to others
-    socket.on('typing', () => {
-      socket.broadcast.emit('typing', {
-        username: socket.username
-      });
-    });
-  
-    // when the client emits 'stop typing', we broadcast it to others
-    socket.on('stop typing', () => {
-      socket.broadcast.emit('stop typing', {
-        username: socket.username
-      });
-    }); 
+
+    socket.on('state', (data) => {
+        switch(data){
+            case Constants.STATE_INIT:
+                // configure door sensor and start messuring
+                startDoorWatching();
+            break;
+            case Constants.STATE_ROOM_READY:
+
+            break;
+            case Constants.STATE_ROOM_ENTERED:
+
+            break;
+            case Constants.STATE_ROOM_ENTERED_UNSEATED:
+
+            break;
+            case Constants.STATE_ROOM_USER_SEATED:
+
+            break;
+            case Constants.STATE_ROOM_USER_SEATED_PHOTO_DONE:
+
+            break;
+            case Constants.STATE_ROOM_TV_ACTIVATED:
+
+            break;
+            case Constants.STATE_ROOM_VIDEO_PLAYING:
+
+            break;
+            case Constants.STATE_ROOM_VIDEO_FINISHED:
+
+            break;
+            case Constants.STATE_ROOM_EXIT_SEQUENCE:
+
+            break;
+        }
+    });     
+
+
+    
   
     // when the user disconnects.. perform this
     socket.on('disconnect', () => {
-      
+        console.log("server disconnected.")
     });
 });
+
+
+function startDoorWatching(){
+    console.log("---->>>>")
+    sockRef.broadcast.emit('state changed', Constants.STATE_ROOM_READY);
+}
--- R1/public/main.js
+++ R1/public/main.js
@@ -12,6 +12,9 @@ $(function() {
         STATE_ROOM_VIDEO_FINISHED               : "ROOM VIDEO FINISHED",
         STATE_ROOM_EXIT_SEQUENCE                : "ROOM EXIT SEQUENCE",
 
+        SENSOR_DOOR_TRIGGERED                   : "DOOR SENSOR TRIGGERED",
+        SENSOR_SEATING_TRIGGERED                : "SEATING SENSOR TRIGGERED",
+
         API_BASE_PATH                           : "",
         MODE_DEBUG                              : true,
 
@@ -52,9 +55,19 @@ $(function() {
 
     var socket = io();
     var connected = false;
+    var userSeated = false;
     
     // load video
     var player = new Clappr.Player({source: "./video/HKFA 29th Pt3.mp4", parentId: "#player",width:"100%", height:"100%"});
+
+    // Audio parameters
+    var defaultAudio = 0.3;
+    var currentVolume = defaultAudio;
+    var audio_state = ""
+    var context = new AudioContext();
+    context.resume();
+    var playlist = [Constants.TRACK_PLEASE_SIT_ROOM_1, Constants.TRACK_ACTIVATE_TV_ROOM_1, Constants.TRACK_PLEASE_EXIT_ROOM_1]
+    var sound = new Howl({ src: [playlist], volume: currentVolume });
 
     // startup activities
     if(Constants.MODE_DEBUG){ $debug.show() }else{ $debug.hide() }
@@ -80,7 +93,9 @@ $(function() {
             case Constants.STATE_INIT:
                 // Check that all is setup and ready to go: 
                 // If ready - move to next step ->
+                context.resume()
                 setTimeout(function(){ setState(Constants.STATE_ROOM_READY) }, 3000);
+                socket.emit('state', Constants.STATE_INIT);
             break;
             case Constants.STATE_ROOM_READY:
                 // init Door sensor and start listening for entrants.
@@ -88,12 +103,13 @@ $(function() {
             break;
             case Constants.STATE_ROOM_ENTERED:
                 // Audio to prompt user to sit down.
-
+                playAudio(Constants.TRACK_PLEASE_SIT_ROOM_1, currentVolume);
+                audio_state = Constants.AUDIO_PLEASE_SIT_ROOM_1;
                 // start listening for seating sensor to trigger
 
                 // once audio is played -> if no trigger is tripped, start the audio prompting loop with increased audio for each iteration.
 
-                setTimeout(function(){ setState(Constants.STATE_ROOM_ENTERED_UNSEATED) }, 3000); // temp
+                // setTimeout(function(){ setState(Constants.STATE_ROOM_ENTERED_UNSEATED) }, 3000); // temp
             break;
             case Constants.STATE_ROOM_ENTERED_UNSEATED:
                 // manage prompting cycle and audio volume increase
@@ -124,47 +140,85 @@ $(function() {
     }
 
 
+
     // FUDGE THE START
-    setState(Constants.STATE_INIT);
+    // setState(Constants.STATE_INIT);
 
     // --------- Audio Handling ------------------------
 
     function playAudio(track, volume){
-        var sound = new Howl({
-            src: [track],
-            volume: volume,
-        });
         
-        sound.once('load', function(){
-            sound.play();
-        });
+            Howler.volume(volume);  
+
+            sound = new Howl({
+                src: [track],
+                volume: volume,
+            });
+            
+            sound.once('load', function(){
+                    console.log('Playback resumed successfully');
+                    sound.volume = volume;
+                    sound.play();
+            });
+            
+            sound.on('end', function(){
+                sound.unload()
+                console.log('Finished!');
+                // handle next depending on state
+                if(audio_state==Constants.AUDIO_PLEASE_SIT_ROOM_1 && currentState==Constants.STATE_ROOM_ENTERED){
+                    setState(Constants.STATE_ROOM_ENTERED_UNSEATED);
+                }
+    
+                console.log("currentState "+currentState)
+                if(currentState==Constants.STATE_ROOM_ENTERED_UNSEATED && currentVolume<1){
+                    currentVolume = currentVolume+0.1
+                    setTimeout(function(){ playAudio(Constants.TRACK_PLEASE_SIT_ROOM_1, currentVolume) }, 1000 );
+                }
+            });
+
         
-        sound.on('end', function(){
-            console.log('Finished!');
-            // handle next depending on state
-        });
     }
 
 
     // ----- Socket handling ---------------------------
 
+    socket.on('sensor event', (data) => {
+        switch(data.event){
+            case Constants.SENSOR_DOOR_TRIGGERED:
+                setState(Constants.STATE_ROOM_ENTERED);
+            break;
+            case Constants.SENSOR_SEATING_TRIGGERED:
+                setState(Constants.STATE_ROOM_USER_SEATED);
+            break;
+        }
+        console.log("SOCKET _ SENSOR EVENT: "+data.event)
+    });
+
     socket.on('state changed', (data) => {
         setState(data);
-      });
+    });
+
+    socket.on('init', (data) => {
+        console.log("init")
+        setState(Constants.STATE_INIT);
+    });
 
     socket.on('disconnect', () => {
-        log('you have been disconnected');
+        console.log('you have been disconnected');
+        connected = false;
       });
     
       socket.on('reconnect', () => {
-        log('you have been reconnected');
-        if (username) {
-          socket.emit('add user', username);
-        }
+        console.log('you have been reconnected');
+        connected = true;
+        // If connected is true
+        socket.emit('startup', "init");
+        setState(Constants.STATE_INIT);
       });
     
       socket.on('reconnect_error', () => {
-        log('attempt to reconnect has failed');
+        console.log('attempt to reconnect has failed');
+        connected = false;
       });
 
 });
