77d94d3e4ebf39dd93286ce00a734e48e1470ca3
th0ress0n <per@thoresson.com.au>
2019-06-10 17:55:12 +1000

    removing and adding CEC

--- R1/app.js
+++ R1/app.js
@@ -27,16 +27,46 @@ var Constants = {
 
 // CEC utils - Required to detect TV on and off via HDMI ----------------
 // ----------------------------------------------------------------------
-var cecRemote = require('hdmi-cec').Remote;
-var remote = new cecRemote();
+var nodecec = require( 'node-cec' );
+var NodeCec = nodecec.NodeCec;
+var CEC     = nodecec.CEC;
 
-remote.on('keypress', function(evt) {
-    console.log('user pressed the key "'+ evt.key + '" with code "' + evt.keyCode + '"');
-});
+var cec = new NodeCec( 'node-cec-monitor' );
 
-remote.on('keypress.select', function() {
-    console.log('user pressed the select key!');
-});
+//- KILL CEC-CLIENT PROCESS ON EXIT
+process.on( 'SIGINT', function() {
+    if ( cec != null ) {
+      cec.stop();
+    }
+    process.exit();
+  });
+  
+  //- CEC EVENT HANDLING
+  cec.once( 'ready', function(client) {
+    console.log( ' -- READY -- ' );
+    client.sendCommand( 0xf0, CEC.Opcode.GIVE_DEVICE_POWER_STATUS );
+  });
+  
+  cec.on( 'REPORT_POWER_STATUS', function (packet, status) {
+    var keys = Object.keys( CEC.PowerStatus );
+    for (var i = keys.length - 1; i >= 0; i--) {
+      if (CEC.PowerStatus[keys[i]] == status) {
+        console.log('POWER_STATUS:', keys[i]);
+        break;
+      }
+    }
+  });
+  
+  cec.on( 'ROUTING_CHANGE', function(packet, fromSource, toSource) {
+    console.log( 'Routing changed from ' + fromSource + ' to ' + toSource + '.' );
+  });
+  
+  //- START CEC CLIENT
+  // -m  = start in monitor-mode
+  // -d8 = set log level to 8 (=TRAFFIC) (-d 8)
+  // -br = logical address set to `recording device`
+  cec.start( 'cec-client', '-m', '-d', '8', '-b', 'r' );
+
 // End CEC section ------------------------------------------------------
 // ----------------------------------------------------------------------
 
